9f798402fce4d6c95c89bcb85c5953c82f7f23a5
503-888-1044 Martell <drmartell@gmail.com>
2019-10-07 17:16:00 -0700

    completes initial data file

---
+++ src/companies.js
@@ -0,0 +1,75 @@
+// sources: Yahoo! Finance, Clearbit.com
+const JPM = {
+    id: 'JPM',
+    name: 'JPMorgan Chase',
+    image: 'https://logo.clearbit.com/jpmorganchase.com',
+    description: 'JPMorgan Chase & Co. operates as a financial services company worldwide. It operates in four segments: Consumer & Community Banking (CCB), Corporate & Investment Bank (CIB), Commercial Banking (CB), and Asset & Wealth Management (AWM).',
+    category: 'Banks - Global',
+    price: 114.37
+};
+
+const BAC = {
+    id: 'BAC',
+    name: 'Bank of America',
+    image: 'https://logo.clearbit.com/bankofamerica.com',
+    description: 'Bank of America Corporation, through its subsidiaries, provides banking and financial products and services for individual consumers, small- and middle-market businesses, institutional investors, large corporations, and governments worldwide.',
+    category: 'Banks - Global',
+    price: 28.31
+};
+
+const AAPL = {
+    id: 'AAPL',
+    name: 'Apple',
+    image: 'https//logo.clearbit.com/apple.com',
+    description: 'Apple Inc. designs, manufactures, and markets mobile communication and media devices, and personal computers.',
+    category: 'Consumer Electronics',
+    price: 227.06
+};
+
+const WFC = {
+    id: 'WFC',
+    name: 'Wells Fargo',
+    image: 'https//logo.clearbit.com/wellsfargo.com',
+    description: 'Wells Fargo & Company, a diversified financial services company, provides retail, commercial, and corporate banking services to individuals, businesses, and institutions.',
+    category: 'Banks - Global',
+    price: 48.81
+};
+
+const ATT = {
+    id: 'ATT',
+    name: 'AT&T',
+    image: 'https//logo.clearbit.com/att.com',
+    description: 'AT&T Inc. provides telecommunication, media, and technology services worldwide.',
+    category: 'Telecom Services',
+    price: 37.66
+};
+
+const C = {
+    id: 'C',
+    name: 'Citigroup',
+    image: 'https//logo.clearbit.com/citigroup.com',
+    description: 'Citigroup Inc., a diversified financial services holding company, provides various financial products and services for consumers, corporations, governments, and institutions in North America, Latin America, Asia, Europe, the Middle East, and Africa.',
+    category: 'Banks - Global',
+    price: 68.15
+};
+
+const MSFT = {
+    id: 'MSFT',
+    name: 'Microsoft',
+    image: 'https//logo.clearbit.com/microsoft.com',
+    description: 'Microsoft Corporation develops, licenses, and supports software, services, devices, and solutions worldwide.',
+    category: 'Software - Infrastructure',
+    price: 137.12
+};
+
+const companiesArr = [
+    JPM,
+    BAC,
+    AAPL,
+    WFC,
+    ATT,
+    C,
+    MSFT
+];
+
+export default companiesArr;
