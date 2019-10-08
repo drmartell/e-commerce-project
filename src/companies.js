e0227898bcb7cd79ed61e2a84277e79a1002e981
503-888-1044 Martell <drmartell@gmail.com>
2019-10-07 20:18:49 -0700

    HTML generator passing initial test

--- src/companies.js
+++ src/companies.js
@@ -20,7 +20,7 @@ const BAC = {
 const AAPL = {
     id: 'AAPL',
     name: 'Apple',
-    image: 'https//logo.clearbit.com/apple.com',
+    image: 'https://logo.clearbit.com/apple.com',
     description: 'Apple Inc. designs, manufactures, and markets mobile communication and media devices, and personal computers.',
     category: 'Consumer Electronics',
     price: 227.06
@@ -29,7 +29,7 @@ const AAPL = {
 const WFC = {
     id: 'WFC',
     name: 'Wells Fargo',
-    image: 'https//logo.clearbit.com/wellsfargo.com',
+    image: 'https://logo.clearbit.com/wellsfargo.com',
     description: 'Wells Fargo & Company, a diversified financial services company, provides retail, commercial, and corporate banking services to individuals, businesses, and institutions.',
     category: 'Banks - Global',
     price: 48.81
@@ -38,7 +38,7 @@ const WFC = {
 const ATT = {
     id: 'ATT',
     name: 'AT&T',
-    image: 'https//logo.clearbit.com/att.com',
+    image: 'https://logo.clearbit.com/att.com',
     description: 'AT&T Inc. provides telecommunication, media, and technology services worldwide.',
     category: 'Telecom Services',
     price: 37.66
@@ -47,7 +47,7 @@ const ATT = {
 const C = {
     id: 'C',
     name: 'Citigroup',
-    image: 'https//logo.clearbit.com/citigroup.com',
+    image: 'https://logo.clearbit.com/citigroup.com',
     description: 'Citigroup Inc., a diversified financial services holding company, provides various financial products and services for consumers, corporations, governments, and institutions in North America, Latin America, Asia, Europe, the Middle East, and Africa.',
     category: 'Banks - Global',
     price: 68.15
@@ -56,7 +56,7 @@ const C = {
 const MSFT = {
     id: 'MSFT',
     name: 'Microsoft',
-    image: 'https//logo.clearbit.com/microsoft.com',
+    image: 'https://logo.clearbit.com/microsoft.com',
     description: 'Microsoft Corporation develops, licenses, and supports software, services, devices, and solutions worldwide.',
     category: 'Software - Infrastructure',
     price: 137.12
