40952d90f8b93334a78ab7462368ec9221911850
503-888-1044 Martell <drmartell@gmail.com>
2019-10-08 17:10:14 -0700

    Step 2: Cart Data complete

---
+++ data/cart.js
@@ -0,0 +1,15 @@
+const cart = [{
+    id: 'JPM',
+    quantity: 100
+}, {
+    id: 'BAC',
+    quantity: 500
+}, {
+    id: 'AAPL',
+    quantity: 1000
+}, {
+    id: 'WFC',
+    quantity: 250
+}];
+
+export default cart;
